7a2de98706d736ff7369401420eead32c7cbd156
Gabriela Bloch <gabriela.bloch@microsoft.wsei.edu.pl>
2023-07-16 17:30:10 +0200

    Add security using identity

---
+++ HRManagement.Application/Contracts/Identity/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using HRManagement.Application.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagement.Application.Contracts.Identity
+{
+    public interface IUserService
+    {
+        public interface IUserService
+        {
+            Task<List<Employee>> GetEmployees();
+            Task<Employee> GetEmployee(string userId);
+        }
+    }
+}
